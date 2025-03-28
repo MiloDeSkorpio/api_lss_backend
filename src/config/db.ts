8e1ffe8769055acb490313b42a551bb137325227
unknown <arcadio.2es0@gmail.com>
2025-03-28 10:50:41 -0600

    dbConnect

---
+++ src/config/db.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize'
+import colors from 'colors'
+import { exit } from 'process'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const connexion = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+  dialect: 'mysql', 
+  host: process.env.DB_HOST, 
+  port: parseInt(process.env.DB_PORT || '3306', 10), 
+  logging: false,
+})
+
+
+export const connectDB = async () => {
+  try {
+    await connexion.authenticate()
+    console.log(colors.magenta.bold(`MySQL conectado en: ${process.env.DB_HOST}:${process.env.DB_PORT}`))
+  } catch (error) {
+    console.log(colors.red.bold('Error al conectar a MySQL:'))
+    console.log(error)
+    exit(1)
+  }
+}
+export default connexion
