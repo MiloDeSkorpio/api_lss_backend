b289d5d31febdb9327a413c1f810718b98436156
unknown <arcadio.2es0@gmail.com>
2025-04-23 13:15:25 -0600

    keyPatternToFileValidate

--- src/middleware/uploadFiles.ts
+++ src/middleware/uploadFiles.ts
@@ -1,24 +1,38 @@
-import multer, { FileFilterCallback } from 'multer';
-import { Request } from 'express';
+import multer, { FileFilterCallback } from 'multer'
+import { Request } from 'express'
 
 // Función de validación del nombre del archivo
 const validateFileName = (filename: string): boolean => {
-  const pattern = /^listablanca_sams_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/
-  return pattern.test(filename);
-};
+  const patterns = {
+    listanegra: /^listanegra_tarjetas_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{14}\.csv$/,
+    listablanca: /^listablanca_sams_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/,
+    listablanca_cv: /^listablanca_sams_cv_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/,
+    inventario: /^inventario_sams_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/
+  }
 
+  const hasCV = filename.includes('_cv_')
+  const fileType = Object.keys(patterns).find(key => {
+    const isCVType = key.includes('_cv')
+    return (hasCV && isCVType && filename.includes(key.replace('_cv', ''))) || 
+           (!hasCV && !isCVType && filename.includes(key))
+  })
+  
+  if (!fileType) return false
+  
+  return patterns[fileType as keyof typeof patterns].test(filename)
+}
 // Configuración de Multer con validación
 const upload = multer({
-  dest: 'uploads/whitelist',
+  dest: 'uploads',
   fileFilter: (
     req: Request,
     file: Express.Multer.File,
     callback: FileFilterCallback,
   ) => {
     if (!validateFileName(file.originalname)) {
-      return callback(new Error(`Formato de nombre inválido: ${file.originalname}`));
+      return callback(new Error(`Formato de nombre inválido: ${file.originalname}`))
     }
-    callback(null, true); // Aceptar el archivo
+    callback(null, true) // Aceptar el archivo
   },
 })
 
