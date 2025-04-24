016829f1e91bb7a79b2e87a027a31bc6ac2a6d43
unknown <arcadio.2es0@gmail.com>
2025-04-24 14:51:39 -0600

    docs: delete to reduce files

--- src/helpers/compareHexToDec.ts
+++
@@ -1,5 +0,0 @@
-export function sonEquivalentesNum(dec: number, hex: string): boolean {
-  const hexAsDec = parseInt(hex, 16);
-  if (isNaN(hexAsDec)) throw new Error(`Hex inválido: ${hex}`);
-  return hexAsDec === dec;
-}
