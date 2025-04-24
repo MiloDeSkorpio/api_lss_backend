42abd0f95c6576fb11d3c8eb89640e710d89ecbc
unknown <arcadio.2es0@gmail.com>
2025-04-24 14:47:24 -0600

    refactor(middleware): new method to upload files

--- src/middleware/uploadFiles.ts
+++ src/middleware/uploadFiles.ts
@@ -1,29 +1,43 @@
 import multer, { FileFilterCallback } from 'multer'
 import { Request } from 'express'
+import { validateFileName } from '../utils/files'
+import fs from 'fs'
 
-// Función de validación del nombre del archivo
-const validateFileName = (filename: string): boolean => {
-  const patterns = {
-    listanegra: /^listanegra_tarjetas_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{14}\.csv$/,
-    listablanca: /^listablanca_sams_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/,
-    listablanca_cv: /^listablanca_sams_cv_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/,
-    inventario: /^inventario_sams_(altas|bajas|cambios)_[0-9A-Fa-f]{2}_\d{12}\.csv$/
+const getDestination = (filename: string): string => {
+  let folder = 'uploads/otros' // Valor por defecto
+
+  // Lógica para determinar la carpeta
+  if (filename.startsWith('listanegra')) {
+    folder = 'uploads/listanegra'
+  } else if (filename.startsWith('listablanca')) {
+    folder = filename.includes('_cv_') 
+      ? 'uploads/listablanca_cv' 
+      : 'uploads/listablanca'
+  } else if (filename.startsWith('inventario')) {
+    folder = 'uploads/inventario'
   }
 
-  const hasCV = filename.includes('_cv_')
-  const fileType = Object.keys(patterns).find(key => {
-    const isCVType = key.includes('_cv')
-    return (hasCV && isCVType && filename.includes(key.replace('_cv', ''))) || 
-           (!hasCV && !isCVType && filename.includes(key))
-  })
-  
-  if (!fileType) return false
-  
-  return patterns[fileType as keyof typeof patterns].test(filename)
+  // Crear carpeta si no existe (con recursividad por si hay subdirectorios)
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder, { recursive: true })
+  }
+
+  return folder
 }
-// Configuración de Multer con validación
+
+const storage = multer.diskStorage({
+  destination: (req: Request, file: Express.Multer.File, callback) => {
+    const destinationFolder = getDestination(file.originalname)
+    callback(null, destinationFolder)
+  },
+  filename: (req, file, callback) => {
+    // Opcional: Personalizar el nombre del archivo si es necesario
+    callback(null, file.originalname)
+  }
+})
+
 const upload = multer({
-  dest: 'uploads',
+  storage: storage,
   fileFilter: (
     req: Request,
     file: Express.Multer.File,
