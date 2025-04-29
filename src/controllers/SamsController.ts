65646c789b70b07a0a0c430aaf9a311211666e6c
unknown <arcadio.2es0@gmail.com>
2025-04-29 13:48:59 -0600

    feat(sams): add createSamsRecordController

--- src/controllers/SamsController.ts
+++ src/controllers/SamsController.ts
@@ -1,13 +1,12 @@
 import type { Request, Response } from 'express'
-import fs from 'fs'
-import csv from 'csv-parser'
-import stripBomStream from 'strip-bom-stream'
-
+import {  processSingleFile } from '../utils/files'
+import SamsSitp from '../models/SamsSitp'
 
 // Interfaces
 interface MulterRequest extends Request {
   files: Express.Multer.File[]
 }
+
 const REQUIRED_HEADERS = ['sam_id_hex', 'sam_id_dec', 'sam_tipo', 'sam_config', 'llaves_tipo',
   'version_parametros', 'lock_index', 'fecha_produccion',
   'hora_produccion', 'atr', 'samsp_id_hex', 'samsp_version_parametros',
@@ -15,14 +14,35 @@ const REQUIRED_HEADERS = ['sam_id_hex', 'sam_id_dec', 'sam_tipo', 'sam_config', 
   'recibido_por', 'documento_soporte1', 'documento_soporte2',
   'observaciones', 'provider_code']
 
+const PROVIDER_CODES = ['01', '02', '03', '04', '05', '06', '07', '15', '32', '3C', '46', '5A', '64']
+
 export class SamsController {
 
-  static newVersionInventory = async (req: MulterRequest, res: Response) => {
+  static createSamsRecordController = async (req: MulterRequest, res: Response) => {
 
-    if (!req.files || req.files.length === 0) {
+    if (!req.file) {
       return res.status(400).json({ error: 'No se subieron archivos' })
     }
-    console.log(req)
+    const file = req.file
+    try {
+  
+      const fileData = await processSingleFile(file,REQUIRED_HEADERS,PROVIDER_CODES)
 
+      const tableExists = await SamsSitp.sequelize.getQueryInterface().tableExists(SamsSitp.tableName)
+      
+      if (!tableExists) {
+        await SamsSitp.sync()
+        return []
+      }
+        
+      const result = await SamsSitp.bulkCreate(
+        fileData.map(sam => ({
+          ...sam,
+        }))
+      )
+      return res.status(200).json(result)
+    } catch (error) {
+      res.status(500).json({ error: `${error}` })
+    }
   }
 }
