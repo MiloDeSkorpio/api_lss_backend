de301c1ea1149d4c51a8995bd33d939eae3ffd16
unknown <arcadio.2es0@gmail.com>
2025-05-05 18:48:47 -0600

    feat(validation): validation row file to reutilize functions in another files

--- src/middleware/validationRow.ts
+++ src/middleware/validationRow.ts
@@ -1,9 +1,8 @@
-import { error } from "console";
 import { ValidationError } from "../utils/files";
 import { genSemoviId, isSamInInventory, isSamValid, normalizeText, validateHexValuesToDec, validateLocationId, validateProviderCode, validateRequiredFields, validateTypeSam } from "../utils/validation";
 
 let ignoreWl = ['ESTACION']
-let ignoreBl = []
+let ignoreBl = ['ESTACION']
 let ignoreIn = ['version_parametros', 'lock_index', 'fecha_produccion', 'hora_produccion', 'atr', 'samsp_id_hex', 'samsp_version_parametros', 'recibido_por', 'documento_soporte1', 'documento_soporte2', 'observaciones']
 
 export function validateRow(row: any, lineNumber: number, validData: any[], errors: ValidationError[], fileName: string, PROVIDER_CODES: string[],samsValid: any[]) {
@@ -38,7 +37,7 @@ async function validateListaBlanca(
   samsValid: any[]
 ) {
   validateRequiredFields(row, ignoreWl)
-  if (!isSamInInventory(samsValid,row.SERIAL_HEX, row.OPERATOR)) {
+  if (!isSamInInventory(samsValid,PROVIDER_CODES,row.SERIAL_HEX, row.OPERATOR)) {
     throw new Error(`El SAM: ${row.SERIAL_HEX} no se encuentra en la DB`)
   }
   const serialDec = validateHexValuesToDec(row.SERIAL_DEC, row.SERIAL_HEX)
