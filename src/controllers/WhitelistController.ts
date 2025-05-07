e883d36a232d90069e47d19bdca77efd46c3fa42
unknown <arcadio.2es0@gmail.com>
2025-05-07 14:43:34 -0600

    feat(controller): newVersionCV static function to use in routes

--- src/controllers/WhitelistController.ts
+++ src/controllers/WhitelistController.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from 'express'
 import WhiteListCV from '../models/WhiteListCV'
 import { categorizeAllFiles, processFileGroup } from '../utils/files'
-import { getHighestVersionRecords, processVersionUpdate } from '../utils/versions'
+import { getHighestVersionRecords, getMaxVersion, processVersionUpdate } from '../utils/versions'
+
 
 const REQUIRED_HEADERS = ['SERIAL_DEC', 'SERIAL_HEX', 'CONFIG', 'OPERATOR', 'LOCATION_ID', 'ESTACION']
-const PROVIDER_CODES = ['01','02','03','04','05','06','07','15', '32', '3C', '46', '5A', '64']
+const PROVIDER_CODES = ['01', '02', '03', '04', '05', '06', '07', '15', '32', '3C', '46', '5A', '64']
 
 // Interfaces
 interface MulterRequest extends Request {
@@ -21,35 +22,32 @@ export class WhitelistController {
 
     const files = req.files
     const categorizeFiles = categorizeAllFiles(files)
-    
+    const keyField = 'SERIAL_DEC'
     try {
       const currentVersionRecords = await getHighestVersionRecords(WhiteListCV)
-    
+      const currentVersion = await getMaxVersion(WhiteListCV)
       let [altasData, bajasData, cambiosData] = await Promise.all([
-        processFileGroup(categorizeFiles.altasFiles,REQUIRED_HEADERS,PROVIDER_CODES ),
-        processFileGroup(categorizeFiles.bajasFiles,REQUIRED_HEADERS,PROVIDER_CODES ),
-        processFileGroup(categorizeFiles.cambiosFiles,REQUIRED_HEADERS,PROVIDER_CODES )
+        processFileGroup(categorizeFiles.altasFiles, REQUIRED_HEADERS, PROVIDER_CODES),
+        processFileGroup(categorizeFiles.bajasFiles, REQUIRED_HEADERS, PROVIDER_CODES),
+        processFileGroup(categorizeFiles.cambiosFiles, REQUIRED_HEADERS, PROVIDER_CODES)
       ])
 
       if (currentVersionRecords.length < 0) {
         if (bajasData.length > 0 || cambiosData.length > 0) {
           console.warn('Primera versión - Ignorando datos de bajas y cambios')
-          bajasData = []    
-          cambiosData = []  
+          bajasData.length = 0
+          cambiosData.length = 0
         }
-        const newVersion = currentVersionRecords[0].VERSION
-        ? currentVersionRecords[0].VERSION
-        : 0
+        const newVersion = currentVersion + 1
 
         res.status(202).json({ message: "Archivos recibidos. Procesando en segundo plano..." })
 
-        // 2. Ejecutar el bulkCreate después de enviar la respuesta
         process.nextTick(async () => {
           try {
             const result = await WhiteListCV.bulkCreate(
               altasData.map(item => ({
                 ...item,
-                VERSION: <number>newVersion + 1
+                VERSION: newVersion
               }))
             )
             console.log(` Inserción exitosa: ${result.length} registros añadidos.`)
@@ -57,32 +55,28 @@ export class WhitelistController {
             console.error(" Error en inserción en segundo plano:", error.message)
           }
         })
-      }  else {
-        res.status(202).json({ message: "Procesando en segundo plano..." })
-        
+      } else {
+
         process.nextTick(async () => {
-            try {
-                const result = await processVersionUpdate(
-                    currentVersionRecords,
-                    WhiteListCV,
-                    altasData,
-                    bajasData,
-                    cambiosData
-                )
-                console.log(currentVersionRecords.length + altasData.length)
-                console.log(`
-                    Versión ${result.newVersion} creada:
-                    - Nuevos: ${result.stats.altas}
-                    - Bajas: ${result.stats.bajas}
-                    - Cambios: ${result.stats.cambios}
-                    `)
-                    // - Duplicados en altas detectados: ${result.stats.duplicados}
-                    // - Duplicados en bajas detectados: ${result.stats.duplicados}
-                    // - Duplicados en cambios detectados: ${result.stats.duplicados}
-                
-            } catch (error) {
-                console.error("Error en procesamiento en segundo plano:", error.message)
+          try {
+            const result = await processVersionUpdate(
+              currentVersionRecords,
+              WhiteListCV,
+              altasData,
+              bajasData,
+              cambiosData,
+              keyField
+            )
+
+            if (!result.success) {
+              return res.status(500).json({ error: "Problema al Generar la nueva version." })
             }
+
+            return res.status(200).json(result)
+
+          } catch (error) {
+            console.error("Error en procesamiento en segundo plano:", error.message)
+          }
         })
       }
 
