163724f65cdbaf2112d8cddf449dc34179aa7a3c
unknown <arcadio.2es0@gmail.com>
2025-05-26 18:41:33 -0600

    feat(routes): add routes for whitelist whit method post and get

--- src/routes/whitelistRoutes.ts
+++ src/routes/whitelistRoutes.ts
@@ -5,11 +5,18 @@ import { uploadCSVs } from "../middleware/uploadFiles";
 const router = Router()
 
 router.get('/last-version-cv',
+  WhitelistController.getLastVersionRecordsCV
+)
+router.get('/last-version',
   WhitelistController.getLastVersionRecords
 )
 router.post('/new-version-cv',
   uploadCSVs,
   WhitelistController.newVersionCV
+)
+router.post('/new-version',
+  uploadCSVs,
+  WhitelistController.newVersion
 )
 
 export default router
