377c1510a63e308abf96b1398b8cb9de972899d1
unknown <arcadio.2es0@gmail.com>
2025-05-26 18:40:18 -0600

    feat(route): add route with method get for all records

--- src/routes/samsRoutes.ts
+++ src/routes/samsRoutes.ts
@@ -4,6 +4,10 @@ import { SamsController } from "../controllers/SamsController";
 
 const router = Router()
 
+router.get('/all-records',
+  SamsController.getAllRecords
+)
+
 router.post('/new-version',
   uploadCSV,
   SamsController.createSamsRecordController
