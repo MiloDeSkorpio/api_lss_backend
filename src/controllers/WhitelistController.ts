866050259ab383e51cb76f5a98162dda8c298654
unknown <arcadio.2es0@gmail.com>
2025-06-05 11:56:03 -0600

    feat(samsByFile): add new method to obtain all sams by one file

--- src/controllers/WhitelistController.ts
+++ src/controllers/WhitelistController.ts
@@ -1,9 +1,13 @@
+import fs from 'fs'
+import csv from 'csv-parser'
+import stripBomStream from 'strip-bom-stream'
 import type { Request, Response } from 'express'
 import WhiteListCV from '../models/WhiteListCV'
 import { categorizeAllFiles, processFileGroup } from '../utils/files'
 import { getHighestVersionRecords, getMaxVersion, processVersionUpdate } from '../utils/versions'
 import WhiteList from '../models/WhiteList'
 import { searchByHexID } from '../utils/buscador'
+import { Op } from 'sequelize'
 
 const REQUIRED_HEADERS = ['SERIAL_DEC', 'SERIAL_HEX', 'CONFIG', 'OPERATOR', 'LOCATION_ID', 'ESTACION']
 const PROVIDER_CODES = ['01', '02', '03', '04', '05', '06', '07', '15', '32', '3C', '46', '5A', '64']
@@ -16,21 +20,95 @@ interface MulterRequest extends Request {
 export class WhitelistController {
 
   static getSamCvByID = async (req: Request, res: Response) => {
-    const { hexId } = req.params 
-    const result = await searchByHexID(hexId,WhiteListCV) 
-    
-    return res.status(200).send(result) 
-  }
+    const { hexId } = req.params
+    const result = await searchByHexID(hexId, WhiteListCV)
 
+    return res.status(200).send(result)
+  }
+  static getSamsCvByID = async (req: MulterRequest, res: Response) => {
+    const resultados: any[] = []
+
+    try {
+      // Validar que el archivo existe
+      if (!req.file || !req.file.path) {
+        throw new Error('No se proporcionó archivo o la ruta es inválida')
+      }
+
+      const batchSize = 100
+      let currentBatch = []
+
+      const processBatch = async (batch) => {
+        const serials = batch.map(row => row.serial_hex)
+
+        const foundRecords = await WhiteListCV.findAll({
+          where: { SERIAL_HEX: { [Op.in]: serials } },
+          raw: true
+        })
+
+        const recordsMap = new Map()
+        foundRecords.forEach(record => {
+          recordsMap.set(record.SERIAL_HEX, record)
+        })
+        // Combinar datos del CSV con los registros encontrados
+        batch.forEach(row => {
+          const matchedRecord = recordsMap.get(row.serial_hex)
+          if (matchedRecord) {
+            resultados.push({
+              ...matchedRecord,      // Todos los campos del registro encontrado
+            })
+          }
+        })
+      }
+      // Procesar el CSV
+      await new Promise((resolve, reject) => {
+        fs.createReadStream(req.file.path)
+          .pipe(stripBomStream())
+          .pipe(csv())
+          .on('data', async (row) => {
+            currentBatch.push(row)
+            if (currentBatch.length >= batchSize) {
+              await processBatch(currentBatch)
+              currentBatch = []
+            }
+
+          })
+          .on('end', async () => {
+            if (currentBatch.length > 0) {
+              const resultados = await processBatch(currentBatch)
+              resolve(resultados)
+            }
+          })
+          .on('error', (error) => {
+            reject(error)
+          })
+      })
+
+      res.json(resultados)
+    } catch (error) {
+      console.error('Error en el procesamiento:', error)
+      res.status(500).json({
+        success: false,
+        error: error.message
+      })
+    } finally {
+      // Limpiar: eliminar el archivo temporal si es necesario
+      if (req.file && req.file.path) {
+        fs.unlink(req.file.path, (err) => {
+          if (err) console.error('Error al eliminar archivo temporal:', err)
+        })
+      }
+    }
+
+  }
   static getLastVersionRecordsCV = async (req: Request, res: Response) => {
     const result = await getHighestVersionRecords(WhiteListCV)
-    
+
     const transformedResult = result.map(record => {
       return Object.fromEntries(
         Object.entries(record).map(([key, value]) => [key.toLowerCase(), value])
       )
     })
-    
+
     res.status(200).json(transformedResult)
   }
   static getLastVersionRecords = async (req: Request, res: Response) => {
@@ -41,7 +119,7 @@ export class WhitelistController {
         Object.entries(record).map(([key, value]) => [key.toLowerCase(), value])
       )
     })
-    
+
     res.status(200).json(transformedResult)
   }
 
