a523581d59880bb7ca3f4596beb81a948f5d8894
unknown <arcadio.2es0@gmail.com>
2025-07-09 19:01:40 -0600

    feat: add new handler to manage errors on upload files by multer

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 import { validationResult } from "express-validator"
+import multer from 'multer'
 
 export const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
     let errors = validationResult(req)
@@ -7,4 +8,13 @@ export const handleInputErrors = (req: Request, res: Response, next: NextFunctio
       return res.status(400).json({errors: errors.array()})
     }
   next()
+}
+
+export function multerErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ error: err.message })
+  } else if (err) {
+    return res.status(400).json({ error: err.message })
+  }
+  next();
 }
