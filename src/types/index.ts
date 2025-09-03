12f0d6fdae5163e39524c5e6276c56ed9a15b5de
unknown <arcadio.2es0@gmail.com>
2025-09-03 11:54:05 -0600

    feat: add new types for Blacklist and StolenCards Models

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,18 @@
 export interface MulterRequest extends Request {
   files: Express.Multer.File[]
 }
+
+export interface StolenCardsAttributes {
+  card_type: string
+  card_serial_number: string
+  date: Date
+}
+
+export interface BlackListAttributtes {
+  card_type: string
+  card_serial_number: string
+  priority: string
+  blacklisting_date: Date
+  version: number
+  estado: boolean
+}
