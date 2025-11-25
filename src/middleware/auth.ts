a3da7de2ece3f57da8b59243dbc28fb66c7b56a8
unknown <arcadio.2es0@gmail.com>
2025-11-25 17:57:44 -0600

    feat: add middleware for auth function

---
+++ src/middleware/auth.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req: Request & { user?: any }, res: Response, next: NextFunction) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).json({ message: "No token provided" });
+
+  try {
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+
+    req.user = decoded
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token inválido" })
+  }
+}
