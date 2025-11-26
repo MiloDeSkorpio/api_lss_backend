1f7235f5e72551b1deb735721abad6dba0084181
unknown <arcadio.2es0@gmail.com>
2025-11-26 13:22:28 -0600

    fix: change decoded variable and text if token not exist

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,13 +2,10 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req: Request & { user?: any }, res: Response, next: NextFunction) => {
-  const token = req.cookies?.token;
-  if (!token) return res.status(401).json({ message: "No token provided" });
-
+  const token = req.cookies?.token
+  if (!token) return res.status(401).json({ message: "No autorizado — token faltante" });
   try {
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
-
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "")
     req.user = decoded
     next();
   } catch (err) {
