e9f91b67e482920e1c1d831fc371dfabc64ad4dd
unknown <arcadio.2es0@gmail.com>
2025-11-25 18:10:35 -0600

    feat: add dto´s for validate auth functions

---
+++ src/dtos/LoginUserDto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsNotEmpty } from 'class-validator'
+
+export class LoginUserDto {
+  @IsNotEmpty({message: 'El correo electrónico es obligatorio'})
+  @IsEmail({}, {message: 'El correo electrónico no es válido'})
+  email: string
+  @IsNotEmpty({message: 'La contraseña es obligatoria'})
+  password: string
+}
