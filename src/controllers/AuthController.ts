004e3ed7c6a8a1bf39e81f7d9435bbedb197fc0b
unknown <arcadio.2es0@gmail.com>
2025-12-03 11:25:09 -0600

    feat: add functions veridy, resend-code and refactor login for autentication account by email

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -6,37 +6,65 @@ const service = new AuthService()
 export class AuthController {
   register = async (req: Request, res: Response) => {
     try {
-      const payload: {name: string, email: string, password: string} = req.body
-      const user = await service.register(payload)
-      res.status(201).json({message: 'Usuario Registrado', user })
+      const payload: { name: string, email: string, password: string } = req.body
+      await service.register(payload)
+      res.status(201).json({ message: 'Usuario registrado. Revisa tu correo para confirmar tu cuenta.' })
     } catch (error: any) {
       res.status(400).json({ message: error.message || "Error al registrar el usuario" })
-    } 
+    }
   }
 
-login = async (req: Request, res: Response) => {
-  try {
-    const payload: { email: string; password: string } = req.body;
-    const { token, user } = await service.login(payload);
+  verify = async (req: Request, res: Response) => {
+    try {
+      const { email, code } = req.body
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: Boolean(process.env.COOKIE_SECURE),
-      sameSite: process.env.COOKIE_SAMESITE as "lax" | "strict" | "none" | undefined || "lax",
-      maxAge: 1000 * 60 * 60 * 24, // 1 día
-    });
+      await service.verifyEmail(email, code)
 
-    return res.status(200).json({
-      message: "Autenticado",
-      user,
-    });
-  } catch (error: any) {
-    return res.status(400).json({
-      message: error.message || "Error al iniciar sesión",
-    })
+      return res.json({ message: "Correo verificado correctamente" })
+
+    } catch (error: any) {
+      console.log(error)
+      return res.status(400).json({ message: error.message })
+    }
   }
-}
-  
+
+  resendCode = async (req: Request, res: Response) => {
+    try {
+      const { email } = req.body;
+
+      await service.resendVerificationCode(email);
+      return res.json({
+        message: "Se ha enviado un nuevo código de verificación",
+      });
+
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  login = async (req: Request, res: Response) => {
+    try {
+      const payload: { email: string; password: string } = req.body;
+      const { token, user } = await service.login(payload);
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: Boolean(process.env.COOKIE_SECURE),
+        sameSite: process.env.COOKIE_SAMESITE as "lax" | "strict" | "none" | undefined || "lax",
+        maxAge: 1000 * 60 * 60 * 24, // 1 día
+      });
+
+      return res.status(200).json({
+        message: "Autenticado",
+        user,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        message: error.message || "Error al iniciar sesión",
+      })
+    }
+  }
+
   me = async (req: Request, res: Response) => {
     try {
       const userId = (req as any).user?.id
@@ -44,7 +72,7 @@ login = async (req: Request, res: Response) => {
         return res.status(401).json({ message: "Usuario no autenticado" })
       }
       const data = await service.me(userId)
-      res.status(200).json({user: data })
+      res.status(200).json({ user: data })
     } catch (error: any) {
       res.status(400).json({ message: error.message || "Error al obtener el usuario" })
     }
