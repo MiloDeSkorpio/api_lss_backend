a75c5401e7b0664c6c423123daa592213504d251
unknown <arcadio.2es0@gmail.com>
2025-12-03 11:30:07 -0600

    feat: add endpoint to verify and resend-code

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,8 +2,9 @@ import { Router } from "express"
 import { AuthController } from "../controllers/AuthController"
 import { validateDto } from "../middleware/validateDto"
 import { RegisterUserDto } from "../dtos/RegisterUserDto"
-import { LoginUserDto } from "../dtos/LoginUserDto"  
+import { LoginUserDto } from "../dtos/LoginUserDto"
 import { authMiddleware } from "../middleware/auth"
+import { VerifyEmailDto } from "../dtos/VerifyEmailDto"
 
 const router = Router()
 const controller = new AuthController()
@@ -11,6 +12,9 @@ const controller = new AuthController()
 router.post('/register', validateDto(RegisterUserDto), controller.register)
 router.post('/login', validateDto(LoginUserDto), controller.login)
 router.post('/logout', authMiddleware, controller.logout)
+router.post('/verify', validateDto(VerifyEmailDto), controller.verify)
+router.post('/resend-code', controller.resendCode)
 router.get('/me', authMiddleware, controller.me)
+
 
 export default router
