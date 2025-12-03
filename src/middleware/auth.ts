7f11f999d4926ed4bae6e08c3b7773265601d54a
unknown <arcadio.2es0@gmail.com>
2025-12-03 12:15:19 -0600

    feat: add decoded variable for check if the loggin account is verified

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,10 +5,14 @@ export const authMiddleware = (req: Request & { user?: any }, res: Response, nex
   const token = req.cookies?.token
   if (!token) return res.status(401).json({ message: "No autorizado — token faltante" });
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || "")
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "") as any
+    if (!decoded.is_verified) {
+      return res.status(401).json({ message: "Debes verificar tu correo antes de continuar" });
+    }
     req.user = decoded
     next();
   } catch (err) {
+    console.error(err)
     return res.status(401).json({ message: "Token inválido" })
   }
 }
