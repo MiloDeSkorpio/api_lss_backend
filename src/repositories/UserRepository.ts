068e6a47247552153b13bdd47d9b146de988b091
unknown <arcadio.2es0@gmail.com>
2025-12-03 11:23:20 -0600

    feat: add updateUser on user repository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,13 +2,26 @@ import User from "../models/User"
 
 export class UserRepository {
   async findByEmail(email: string): Promise<User | null> {
-    return await User.findOne({ where: { email },raw: true })
+    return await User.findOne({ where: { email }, raw: true })
   }
   async findById(id: number): Promise<User | null> {
     return await User.findByPk(id, { attributes: ["id", "name", "email", "roleId"] })
   }
-  async create(userPayload: {name: string, email: string, password: string}): Promise<User> {
+  async create(userPayload: { name: string, email: string, password: string, verification_code: string, verification_expires: Date, is_verified: boolean }): Promise<User> {
     return await User.create(userPayload)
+  }
+  async updateUser(identifier: { id?: number; email?: string },
+    updates: Partial<{
+      name: string
+      email: string
+      password: string
+      verification_code: string
+      verification_expires: Date
+      is_verified: boolean
+    }>
+  ): Promise<number> {
+    const [affectedRows] = await User.update(updates, { where: identifier })
+    return affectedRows
   }
   async clearToken() {
     return true
