493b837f82c3313529099f28fa4dce0506791fa5
unknown <arcadio.2es0@gmail.com>
2025-12-05 17:43:58 -0600

    fix: edit the length for name and password

--- src/dtos/RegisterUserDto.ts
+++ src/dtos/RegisterUserDto.ts
@@ -2,13 +2,13 @@ import { IsEmail, IsNotEmpty, Length} from 'class-validator'
 
 export class RegisterUserDto {
   @IsNotEmpty({message: 'El nombre es obligatorio'})
-  @Length(2, 128, {message: 'El nombre debe tener entre 2 y 128 caracteres'})
+  @Length(3, 128, {message: 'El nombre debe tener entre 3 y 128 caracteres'})
   name: string
   @IsNotEmpty({message: 'El correo electrónico es obligatorio'})
   @IsEmail({}, {message: 'El correo electrónico no es válido'})
   email: string
   @IsNotEmpty({message: 'La contraseña es obligatoria'})
-  @Length(6, 128, {message: 'La contraseña debe tener entre 6 y 128 caracteres'})
+  @Length(8, 128, {message: 'La contraseña debe tener entre 8 y 128 caracteres'})
   password: string 
   
 }
