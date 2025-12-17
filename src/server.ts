6eee0a788c8e3337ac4f089500be1fe94126efca
unknown <arcadio.2es0@gmail.com>
2025-12-16 18:39:51 -0600

    feat: add version history routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import whiteListRoutes from './routes/whitelistRoutes'
 import samsRoutes from './routes/samsRoutes'
 import blackListRoutes from './routes/blacklistRoutes'
+import versionHistoryRoutes from './routes/versionHistoryRoutes' // Import the new routes
 import { connectDB } from './config/db'
 import authRoutes from './routes/authRoutes'
 import cookieParser from 'cookie-parser'
@@ -27,5 +28,6 @@ server.use("/api/auth", authRoutes)
 server.use('/api/blacklist',blackListRoutes)
 server.use('/api/whitelist',whiteListRoutes)
 server.use('/api/sams',samsRoutes)
+server.use('/api/version-history', versionHistoryRoutes)
 
 export default server
