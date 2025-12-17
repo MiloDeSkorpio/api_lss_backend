2a4bb12629ad4f15360cb1e5709faa9e23170da4
unknown <arcadio.2es0@gmail.com>
2025-12-16 18:54:06 -0600

    feat: add the services for history versions

---
+++ src/services/VersionHistoryService.ts
@@ -0,0 +1,27 @@
+import { VersionHistoryRepository } from '../repositories/VersionHistoryRepository'
+import { VersionHistoryAttributes } from '../types'
+import VersionHistory from '../models/VersionHistory'
+
+export class VersionHistoryService {
+  private readonly repository: VersionHistoryRepository
+
+  constructor() {
+    this.repository = new VersionHistoryRepository()
+  }
+
+  async logVersionCreation(data: Omit<VersionHistoryAttributes, 'id' | 'createdAt' | 'updatedAt'>): Promise<VersionHistory> {
+    return await this.repository.create(data as VersionHistoryAttributes)
+  }
+
+  async getVersionsByListName(listName: 'WHITELIST' | 'BLACKLIST' | 'WHITELIST_CV' | 'LSS-TCSM'): Promise<VersionHistory[]> {
+    return await this.repository.findAllByListName(listName)
+  }
+
+  async getLatestVersionByListName(listName: 'WHITELIST' | 'BLACKLIST' | 'WHITELIST_CV' | 'LSS-TCSM'): Promise<VersionHistory | null> {
+    return await this.repository.findLatestVersionByListName(listName)
+  }
+
+  async getVersionById(id: string): Promise<VersionHistory | null> {
+    return await this.repository.findById(id)
+  }
+}
