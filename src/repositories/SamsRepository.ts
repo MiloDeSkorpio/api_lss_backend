106c3cfc53c81ae187a935a1bd90ce9a98b71fa7
unknown <arcadio.2es0@gmail.com>
2025-12-19 18:57:51 -0600

    feat: add function to find samss by serial hex

--- src/repositories/SamsRepository.ts
+++ src/repositories/SamsRepository.ts
@@ -1,5 +1,6 @@
 import SamsSitp from '../models/SamsSitp'
 import { SamsSitpAttributes } from '../types'
+import { Op } from 'sequelize'
 
 
 export class SamsRepository {
@@ -18,5 +19,17 @@ export class SamsRepository {
 
   public async bulkCreate(records: SamsSitpAttributes[]): Promise<any> {
     return await SamsSitp.bulkCreate(records)
+  }
+
+  public async findExistingSerialsByHex(hexSerials: string[]): Promise<SamsSitp[]> {
+    const existingSams = await SamsSitp.findAll({
+      where: {
+        serial_number_hexadecimal: {
+          [Op.in]: hexSerials,
+        },
+      },
+      attributes: ['serial_number_hexadecimal'], // Solo necesitamos este campo para verificar existencia
+    });
+    return existingSams;
   }
 }
