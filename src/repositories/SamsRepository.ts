7b409855d898e810c662c04e8dbcbb3c92142ae0
unknown <arcadio.2es0@gmail.com>
2026-01-06 18:35:14 -0600

    feat: add functions to get last version and last version records

--- src/repositories/SamsRepository.ts
+++ src/repositories/SamsRepository.ts
@@ -1,6 +1,7 @@
 import SamsSitp from '../models/SamsSitp'
 import { SamsSitpAttributes } from '../types'
 import { Op } from 'sequelize'
+import { getHighestVersionRecords, getMaxVersion } from '../utils/versions'
 
 
 export class SamsRepository {
@@ -31,5 +32,15 @@ export class SamsRepository {
       attributes: ['serial_number_hexadecimal'],
     })
     return existingSams
+  }
+
+  public async getLastVersión(): Promise<any> {
+    const latestVersion = getMaxVersion(SamsSitp,'version')
+    return latestVersion
+  }
+
+  public async getLastVersionRecords(): Promise<SamsSitp[]> {
+    const lastVersionRecords = getHighestVersionRecords(SamsSitp,'version','status')
+    return lastVersionRecords
   }
 }
