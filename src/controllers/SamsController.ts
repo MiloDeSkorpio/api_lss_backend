8b6de0650b10dee7fa0cc1394ae5265457439745
unknown <arcadio.2es0@gmail.com>
2026-01-13 13:39:44 -0600

    feat: add summary controller

--- src/controllers/SamsController.ts
+++ src/controllers/SamsController.ts
@@ -40,6 +40,20 @@ export class SamsController {
       return res.status(500).json({ success: false, message: error.message || 'Error interno del servidor' })
     }
   }
+
+  static readonly getSummary = async(req: Request, res: Response) => {
+    try {
+      const result = await SamsController.samsService.getSummaryLastVersión()
+      if(result.success){
+        return res.status(200).json(result)
+      } else {
+        return res.status(400).json(result)
+      }
+    } catch (error) {
+      console.log('Error al obtener resumen:', error)
+      return res.status(500).json({success: false, message: error.message})
+    }
+  }
   
   static getAllRecords = async (req: Request, res: Response) => {
     const tableExists = await SamsSitp.sequelize.getQueryInterface().tableExists(SamsSitp.tableName)
